3183a29233e6435217a1dbbad0a25da6e717a375
AI-CodeLab985 <2530215642@qq.com>
2026-02-02 17:26:23 +0800

    初始化Spring Boot后端项目，配置8081端口，添加Spring web+MySQl Driver依赖

---
+++ src/main/java/com/guruiguo/abysseyebackend/AbyssEyeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.guruiguo.abysseyebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbyssEyeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbyssEyeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guruiguo/abysseyebackend/AbyssEyeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guruiguo.abysseyebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbyssEyeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
